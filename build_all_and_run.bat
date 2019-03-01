9ee2381342d96693387e8b9cf6557e0624db4609
Alan <alanqdev@gmail.com>
2019-03-01 12:37:11 -0300

    v

--- build_all_and_run.bat
+++ build_all_and_run.bat
@@ -1,3 +1,4 @@
+dotnet restore
 FOR /L %%I IN (0,1,63) DO (
    echo "building %%I..."
    dotnet msbuild /t:Clean;Rebuild /p:Platform=x64 /p:Configuration=Release /p:DefineConstants=f%%I /p:OutDir=..\tests\%%I PrimitiveWrapperBenchmarks\PrimitiveWrapperBenchmarks.csproj
