3009510c5ec3408979c30e053d037c10398bec07
Adam Sitnik <adam.sitnik@gmail.com>
2019-03-01 17:42:57 +0100

    tell BDN to run BREAK vs NON BREAK using config

--- PrimitiveWrapperBenchmarks/Program.cs
+++ PrimitiveWrapperBenchmarks/Program.cs
@@ -1,28 +1,37 @@
-﻿#define Break
-
-using System;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        BenchmarkRunner.Run<Repro>();
-        Console.ReadKey();
-    }
+    public static void Main(string[] args) =>
+        BenchmarkRunner.Run<Repro>(
+            DefaultConfig.Instance
+                .With(Job.Default
+                    .WithLaunchCount(1) // how many processes to run
+                    .WithId("NO BREAK"))
+                .With(Job.Default
+                    .WithLaunchCount(1) // how many processes to run
+                    .WithCustomBuildConfiguration("BREAK")
+                    .With(new [] { new MsBuildArgument("/p:Optimize=true") }) // we are using custom build configuration, so we need to tell MSBuild in explicit way to optimize the code
+                    .WithId("BREAK"))
+                // .With(HardwareCounter.InstructionRetired)
+                // .With(new EtwProfiler(new EtwProfilerConfig()))
+                .With(DisassemblyDiagnoser.Create(new DisassemblyDiagnoserConfig(printAsm: true, printPrologAndEpilog: true))));
 }
 
-[DisassemblyDiagnoser]
 public class Repro
 {
     public void f0(){}
     public void f1(){}
     public void f2(){}
     public void f3(){}
-    #if Break
+#if BREAK
     public void f4(){}
-    #endif
+#endif
 
     [Benchmark]
     public int First()
